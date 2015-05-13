07b204b2b2f72e302fbdacf2e32d8a9cdb87c65b
Nacho Decima <jidecima87@gmail.com>
2015-05-13 01:48:42 -0300

    Add layout for Navbar category

---
+++ wp-content/themes/moxie-wiki/html/partial/navbar.php
@@ -0,0 +1,117 @@
+<nav class="navbar">
+	<ul class="accordion">
+		<li>
+			<h3>Architecture</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Coding</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Community</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Ux</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Blog and News</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Services</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Set Up</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Inspiration</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Project Tools</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Elements</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Other Resources</h3>
+			<ul class="sub">
+				<li><a href="">Cms</a></li>
+				<li><a href="">eCommerce</a></li>
+				<li><a href="">Frameworks</a></li>
+				<li><a href="">Grids</a></li>
+				<li><a href="">Preprocessors</a></li>
+			</ul>
+		</li>
+		<li>
+			<h3>Show All</h3>
+		</li>
+	</ul>
+</nav>
