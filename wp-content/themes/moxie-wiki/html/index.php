5a50a255c819c6c7e992f516521fc0707d7fb742
Nacho Decima <jidecima87@gmail.com>
2015-05-15 01:08:58 -0300

    Add layout for pagination.

--- wp-content/themes/moxie-wiki/html/index.php
+++ wp-content/themes/moxie-wiki/html/index.php
@@ -1,13 +1,16 @@
 <?php include './head/head.php' ?>
 
-<?php include './aside/aside.php' ?>
+<div class="row-parent">
+	<?php include './aside/aside.php' ?>
 
-<?php include './header/header.php' ?>
+	<div class="column-parent">
+		<?php include './header/header.php' ?>
 
-<?php include './partial/list-links.php' ?>
+		<?php include './partial/list-links.php' ?>
+	</div>
 
-
-<?php include './partial/footer.php' ?>
+	<?php include './partial/footer.php' ?>
+</div>
 
 
 <?php include './foot/foot.php' ?>
