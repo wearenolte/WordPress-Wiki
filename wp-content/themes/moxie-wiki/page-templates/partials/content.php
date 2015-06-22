f4878f2011c285c030581e67c9aab41612aaa396
Nacho Decima <jidecima87@gmail.com>
2015-06-22 10:43:12 -0300

    Start to integrate , show custom post.

--- wp-content/themes/moxie-wiki/page-templates/partials/content.php
+++ wp-content/themes/moxie-wiki/page-templates/partials/content.php
@@ -4,74 +4,27 @@
  */
 ?>
 
-<?php tha_entry_before(); ?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope itemType="http://schema.org/BlogPosting" >
-	<?php tha_entry_top(); ?>
-	<header class="entry-header">
-
-		<h1 class="entry-title" itemprop="name" ><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
-	<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<span class="genericon genericon-time"></span> <?php moxie_wiki_posted_on(); ?>
-			<span itemprop="dateModified" style="display:none;"><?php printf( __( 'Last modified:', 'some-like-it-neat' ) ); ?> <?php the_modified_date(); ?></span>
-		</div><!-- .entry-meta -->
-	<?php
-endif; ?>
-	</header><!-- .entry-header -->
-
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary" itemprop="description">
-	<?php the_excerpt(); ?>
-	</div><!-- .entry-summary -->
-	<?php else : ?>
-	<div class="entry-content" itemprop="articleBody">
-	<?php
-	the_content(
-		sprintf(
-			__( 'Continue reading%s &rarr;', 'some-like-it-neat' ),
-			'<span class="screen-reader-text">  '.get_the_title().'</span>'
-		)
-	);
-	?>
-
-	</div><!-- .entry-content -->
-	<?php
-endif; ?>
-
-	<footer class="entry-meta" itemprop="keywords">
-	<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
-	<?php
-				/* translators: used between list items, there is a space after the comma */
-				$categories_list = get_the_category_list( __( ', ', 'some-like-it-neat' ) );
-	if ( $categories_list && moxie_wiki_categorized_blog() ) :
-	?>
-	<span class="cat-links">
-	<?php printf( __( 'Posted in %1$s', 'some-like-it-neat' ), $categories_list ); ?>
-	</span>
-	<?php
-	endif; // End if categories ?>
-
-	<?php
-				/* translators: used between list items, there is a space after the comma */
-				$tags_list = get_the_tag_list( '', __( ', ', 'some-like-it-neat' ) );
-	if ( $tags_list ) :
-	?>
-	<span class="tags-links">
-	<?php printf( __( 'Tagged %1$s', 'some-like-it-neat' ), $tags_list ); ?>
-	</span>
-	<?php
-	endif; // End if $tags_list ?>
-	<?php
-	endif; // End if 'post' == get_post_type() ?>
-
-	<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
-		<span class="comments-link" itemprop="comment" ><?php comments_popup_link( __( 'Leave a comment', 'some-like-it-neat' ), __( '1 Comment', 'some-like-it-neat' ), __( '% Comments', 'some-like-it-neat' ) ); ?></span>
-	<?php
-endif; ?>
-
-	<?php edit_post_link( __( 'Edit', 'some-like-it-neat' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
-	<?php tha_entry_bottom(); ?>
-</article><!-- #post-## -->
-<?php tha_entry_after(); ?>
+<?php
+    $loop = new WP_Query( array( 'post_type' => 'link', 'category_name' => 'current', 'ignore_sticky_posts' => 1, 'paged' => $paged ) );
+    if ( $loop->have_posts() ) :
+        while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <div class="pindex">
+                <?php if ( has_post_thumbnail() ) { ?>
+                    <div class="pimage">
+                        <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                    </div>
+                <?php } ?>
+                <div class="ptitle">
+                    <h2><?php echo get_the_title(); ?></h2>
+                </div>
+            </div>
+        <?php endwhile;
+        if (  $loop->max_num_pages > 1 ) : ?>
+            <div id="nav-below" class="navigation">
+                <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Previous', 'domain' ) ); ?></div>
+                <div class="nav-next"><?php previous_posts_link( __( 'Next <span class="meta-nav">&rarr;</span>', 'domain' ) ); ?></div>
+            </div>
+        <?php endif;
+    endif;
+    wp_reset_postdata();
+?>
