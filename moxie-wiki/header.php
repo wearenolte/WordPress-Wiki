f078065878ecfe1b22e289f17462ad1965808749
Nacho Decima <jidecima87@gmail.com>
2015-07-05 21:53:48 -0300

    Integrate with glue navbar and logo desktop.

--- moxie-wiki/header.php
+++ moxie-wiki/header.php
@@ -7,6 +7,8 @@
  * @package moxie_wiki
  */
 ?>
+<?php use \glue\View; ?>
+
 <!DOCTYPE html>
 <?php tha_html_before(); ?>
 <html <?php language_attributes(); ?>>
@@ -31,205 +33,54 @@
 <body <?php body_class(); ?>>
 <?php tha_body_top(); ?>
 
-<div class="flex-child row-parent">
+	<div class="flex-child row-parent">
 
-	<!-- Aside -->
+		<!-- Aside -->
 
-	<aside>
-		<section class="branding">
-			<figure>
+		<aside>
+			<?php
+				$image_url = esc_url( get_theme_mod( 'themeslug_logo' ) );
+			 ?>
+			<?php
+			View::make('shared/logo_desktop')
+			  ->with('site_url', get_site_url())
+			  ->with('site_name', get_bloginfo())
+			  ->with('logo_url', $image_url)
+			  ->render();
+			?>
+
+			<?php
+			View::make('shared/navbar')
+			  ->render();
+			?>
+		</aside>
+
+		<div class="column-parent">
+
+			<!-- Header -->
+			<header class="header flex-child">
 				<?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home'>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home' class="logo-mobile">
 						<img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
 					</a>
 				<?php else : ?>
+					<a href="<?php echo site_url(); ?>"><?php bloginfo(); ?></a>
 				<?php endif; ?>
-
-			</figure>
-			<h1><a href="<?php echo site_url(); ?>"><?php bloginfo(); ?></a></h1>
-		</section>
-
-		<nav class="navbar">
-			<ul class="accordion" data-behavior="accordion">
-				<li class="item-accordion">
-					<h3 class="js-accordion-trigger">
-						Architecture
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
+				<p class="link-count"><span class="link-icon icon-eye"></span>Browsing <span class="link-highlight">646</span> links</p>
+				<div class="search-container" data-behavior="search-toggle">
+					<form class="searchbox" action="http://wwwhere.io/search/results">
+						<input type="text" name="q" placeholder="Search" class="searchbox-input" onkeyup="buttonUp()">
+						<input type="submit" class="searchbox-submit" value="Go">
+						<span class="searchbox-icon icon-search js-search-toggle"></span>
+					</form>
+				</div>
+				<div class="menu-mobile button-default" data-behavior="modal-open"><span class="js-open-modal icon-menu"></span></div>
+				<div class="sharing">
+					<ul class="social-sharing">
+						<li><a href="" class="popup share-twitter"><span class="share-icon icon-twitter"></span> <span class="share-text">tweet</span></a></li>
+						<li><a href="" class="popup share-facebook"><span class="share-icon icon-facebook"></span> <span class="share-text">share</span></a></li>
 					</ul>
-				</li>
-				<li class="item-accordion">
-					<h3 class="js-accordion-trigger">
-						Coding
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li class="item-accordion">
-					<h3 class="js-accordion-trigger">
-						Community
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Ux
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Blog and News
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Services
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Set Up
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Inspiration
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Project Tools
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Elements
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li>
-					<h3 class="js-accordion-trigger">
-						Other Resources
-						<span class="link-icon icon-keyboard-arrow-right"></span>
-					</h3>
-					<ul class="sub">
-						<li><a href="">Cms</a></li>
-						<li><a href="">eCommerce</a></li>
-						<li><a href="">Frameworks</a></li>
-						<li><a href="">Grids</a></li>
-						<li><a href="">Preprocessors</a></li>
-					</ul>
-				</li>
-				<li data-behavior="modal-open">
-					<h3 class="js-open-modal">
-						Show All
-						<span class="link-icon icon-gallery"></span>
-					</h3>
-				</li>
-			</ul>
-		</nav>
-	</aside>
-
-	<div class="column-parent">
-
-		<!-- Header -->
-		<header class="header flex-child">
-			<?php if ( get_theme_mod( 'themeslug_logo' ) ) : ?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>' rel='home' class="logo-mobile">
-					<img src='<?php echo esc_url( get_theme_mod( 'themeslug_logo' ) ); ?>' alt='<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>'>
-				</a>
-			<?php else : ?>
-				<a href="<?php echo site_url(); ?>"><?php bloginfo(); ?></a>
-			<?php endif; ?>
-			<p class="link-count"><span class="link-icon icon-eye"></span>Browsing <span class="link-highlight">646</span> links</p>
-			<div class="search-container" data-behavior="search-toggle">
-				<form class="searchbox" action="http://wwwhere.io/search/results">
-					<input type="text" name="q" placeholder="Search" class="searchbox-input" onkeyup="buttonUp()">
-					<input type="submit" class="searchbox-submit" value="Go">
-					<span class="searchbox-icon icon-search js-search-toggle"></span>
-				</form>
-			</div>
-			<div class="menu-mobile button-default" data-behavior="modal-open"><span class="js-open-modal icon-menu"></span></div>
-			<div class="sharing">
-				<ul class="social-sharing">
-					<li><a href="" class="popup share-twitter"><span class="share-icon icon-twitter"></span> <span class="share-text">tweet</span></a></li>
-					<li><a href="" class="popup share-facebook"><span class="share-icon icon-facebook"></span> <span class="share-text">share</span></a></li>
-				</ul>
-			</div>
-		</header>
+				</div>
+			</header>
 
 
