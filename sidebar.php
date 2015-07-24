eb0324cb5fb81f6fcb1def0183d4aaa5e5c3d276
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-07-24 15:58:18 -0500

    Load the navbar from a Glue view

--- sidebar.php
+++ sidebar.php
@@ -14,13 +14,19 @@ use glue\View;
 	$image_url = esc_url( get_theme_mod( 'themeslug_logo' ) );
 
 	if ( glue_view_exist() ) {
-		View::make('navbar/logo-desktop')
-		->with('site_url', get_site_url())
-		->with('site_name', get_bloginfo())
-		->with('logo_url', $image_url)
-		->render();
+		View::make( 'navbar/logo-desktop' )
+			->with( 'site_url', get_site_url() )
+			->with( 'site_name', get_bloginfo() )
+			->with( 'logo_url', $image_url )
+			->render();
+
+		$categories = get_formated_taxonomies();
+
+		View::make( 'navbar/navbar' )
+			->with( 'categories', $categories )
+			->render();
 	}
-	?>
+?>
 
 	<?php tha_sidebar_bottom(); ?>
 	</aside>
@@ -31,27 +37,27 @@ use glue\View;
 	<?php do_action( 'before_sidebar' ); ?>
 	<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
-	<aside id="search" class="widget widget_search">
-	</aside>
+		<aside id="search" class="widget widget_search">
+		</aside>
 
-	<aside id="archives" class="widget">
-	<h4 class="widget-title"><?php _e( 'Archives', 'some-like-it-neat' ); ?></h4>
-	<ul>
-	<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
+		<aside id="archives" class="widget">
+		<h4 class="widget-title"><?php _e( 'Archives', 'some-like-it-neat' ); ?></h4>
+		<ul>
+		<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 	</ul>
-	</aside>
 
-	<aside id="meta" class="widget">
-	<h4 class="widget-title"><?php _e( 'Meta', 'some-like-it-neat' ); ?></h4>
-	<ul>
-	<?php wp_register(); ?>
+		</aside>
+
+		<aside id="meta" class="widget">
+		<h4 class="widget-title"><?php _e( 'Meta', 'some-like-it-neat' ); ?></h4>
+		<ul>
+		<?php wp_register(); ?>
 	<li><?php wp_loginout(); ?></li>
-	<?php wp_meta(); ?>
+		<?php wp_meta(); ?>
 	</ul>
-	</aside>
+		</aside>
 
-	<?php
-	endif; // end sidebar widget area ?>
-	</div><!-- #secondary -->
+		<?php endif;  ?>
+	</div>
  */
 ?>
