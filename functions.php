3036aa051170cbc3884fe2f5f00b2512f9e22c66
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-07-27 15:59:17 -0500

    Include file to handle meta boxes

--- functions.php
+++ functions.php
@@ -136,6 +136,9 @@ if ( ! function_exists( 'moxie_wiki_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'moxie_wiki_setup' );
 
+// Include link meta boxes
+include 'meta-fields/link-fields.php' ;
+
 /**
  * Enqueue scripts and styles.
  */
--- meta-fields/link-fields.php
+++ meta-fields/link-fields.php
@@ -1,17 +1,19 @@
 <?php
 
-	function add_meta_box_2( $post_type ) {
-		$post_types = array('post', 'link');     //limit meta box to certain post types
-        if ( in_array( $post_type, $post_types )) {
-			add_meta_box(
-				'url'
-				,__( 'Url Link', 'myplugin_textdomain' )
-				,array( $this, 'render_meta_box_content' )
-				,$post_type
-				,'advanced'
-				,'high'
-			);
-        }
-	}
+add_action( 'add_meta_boxes', 'cd_meta_box_add' );
 
-	add_action('add_meta_box','add_meta_box_2');
+function cd_meta_box_add()
+{
+    add_meta_box( 'my-meta-box-id', 'My First Meta Box', 'cd_meta_box_cb', 'post', 'normal', 'high' );
+}
+
+function cd_meta_box_cb() {
+    echo 'What you put here, show\'s up in the meta box';
+}
+
+function cd_meta_box_cb() {
+?>
+    <label for="my_meta_box_text">Text Label</label>
+    <input type="text" name="my_meta_box_text" id="my_meta_box_text" />
+<?php
+}
