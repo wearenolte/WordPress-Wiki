f6b0f6fb7acc2ebc6a5b92a0120b3fd7a327b110
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-08-05 11:51:59 -0500

    Indent the code of the the index page correctly.

--- index.php
+++ index.php
@@ -5,9 +5,8 @@
  * @package moxie_wiki
  */
 
-get_header(); ?>
+get_header();
 
-<?php
 $paged = get_query_var('paged') ? get_query_var('paged') : 1;
 $args = array(
 	'post_type' => LINKS_POST_TYPE,
@@ -15,24 +14,20 @@ $args = array(
 	'paged' => $paged
 );
 $links = new WP_Query( $args );
-?>
-		<?php if ( $links->have_posts() ) : ?>
+if ( $links->have_posts() ) :
 
-		<?php while ( $links->have_posts() ) : $links->the_post(); ?>
-		<?php
+	while ( $links->have_posts() ) : $links->the_post();
 		if ( glue_view_exist() ){
 			glue\View::make('link/single')
 				->with('title', get_the_title())
 				->with('link', wp_get_shortlink())
 				->render();
 		}
-		?>
-		<?php endwhile; ?>
+	endwhile;
 
-		<?php else : ?>
+else :
+	get_template_part( 'page-templates/partials/content', 'none' );
+endif;
 
-		<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-<?php get_footer(); ?>
+get_footer();
+?>
