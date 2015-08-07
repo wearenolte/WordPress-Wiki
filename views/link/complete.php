3623fbbec1a298368f57c0087043adb20bd59439
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-08-06 20:05:36 -0500

    Display the complete template only if has content
    Test if the content variable has been set before show any content to the
    page, single link view page in this case.

--- views/link/complete.php
+++ views/link/complete.php
@@ -1,7 +1,16 @@
-<?php
-
-if( class_exists('Parsedown') ){
-	$Parsedown = new Parsedown();
-	$content = $Parsedown->text( $content );
-}
-printf('%s', $content);
+<?php if( $content ): ?>
+<section class="list-links flex-child">
+	<div class="link single-link">
+		<div class="link-color" style="background:#F08484"></div>
+		<div class="link-content">
+			<?php
+			if( class_exists('Parsedown') ){
+				$Parsedown = new Parsedown();
+				$content = $Parsedown->text( $content );
+			}
+			printf('%s', $content);
+			?>
+		</div>
+	</div>
+</section>
+<?php endif; ?>
