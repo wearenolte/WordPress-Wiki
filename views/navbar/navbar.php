024c1fd1bdc5bc0ac5adfee96f34090c64ef3b08
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-08-07 10:30:12 -0500

    Update markup to add links to the items wiouth childs
    Add links to the elements in the navbar that has not childs elements
    inside of it, so the links wiouth a child are going to have a direct
    link instead.

--- views/navbar/navbar.php
+++ views/navbar/navbar.php
@@ -6,16 +6,24 @@
 		foreach( $categories as $parent ):
 			extract( $parent );
 			$childs = isset( $childs ) ? $childs : array();
-			$arrow_class = empty( $childs ) ? '' : 'icon-keyboard-arrow-right'
+			$arrow_class = '';
+			if( empty( $childs) ){
+				$title = sprintf('<a class="parent no-childs" href="%s" title="%s"><h3>%s</h3></a>',
+					$link, $title, $title
+				);
+			} else {
+				$arrow_class = 'icon-keyboard-arrow-right';
+				$title = sprintf('<h3 class="js-accordion-trigger">%s<span class="link-icon %s"></span></h3>',
+					$title, $arrow_class
+				);
+			}
 		?>
 
-		<li class="item-accordion">
-			<h3 class="js-accordion-trigger"><?php echo $title; ?>
-				<span class="link-icon <?php echo $arrow_class; ?>"></span>
+			<li class="item-accordion">
+				<?php echo $title; ?>
 			</h3>
 			<?php if( ! empty( $childs ) ): ?>
 			<ul class="sub">
-
 			<?php foreach ( $childs as $child ) : ?>
 				<?php extract( $child ); ?>
 				<li><a href="<?php echo $link; ?>"><?php echo $title; ?></a></li>
