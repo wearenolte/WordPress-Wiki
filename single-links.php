0b42c07c696999e4caa25ec9e539b205fc5acf14
Crisoforo Gaspar Hernandez <mitogh@gmail.com>
2015-08-06 20:04:59 -0500

    Update single view of a link
    This allows to use only php tags removing any html tag from the single
    view of a link page.

--- single-links.php
+++ single-links.php
@@ -7,32 +7,21 @@ use glue\View;
  */
 
 get_header();
-?>
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content">
+	while ( have_posts() ) : the_post();
 
-    <?php while ( have_posts() ) : the_post(); ?>
-
-		<?php
 		$content = get_the_content();
 		View::make('link/complete')
 			->with('content', $content)
 			->render();
-		?>
 
-    <?php
 				// If comments are open or we have at least one comment, load up the comment template
 	if ( comments_open() || '0' != get_comments_number() ) :
 		comments_template();
 	endif;
-	?>
 
-    <?php
-endwhile; // end of the loop. ?>
+endwhile;
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+get_sidebar();
+get_footer();
+?>
