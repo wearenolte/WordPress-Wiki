0f4cc89f9b7f55dfd222685d7ddde8c905168204
Nacho Decima <jidecima87@gmail.com>
2015-08-11 17:45:26 -0300

    Add title to single view post.

--- views/link/complete.php
+++ views/link/complete.php
@@ -3,6 +3,9 @@
 	<div class="link single-link">
 		<div class="link-color" style="background:#F08484"></div>
 		<div class="link-content">
+			<h2><?php the_title(); ?></h2>
+		</div>
+		<div class="link-content">
 			<?php
 			if( class_exists('Parsedown') ){
 				$Parsedown = new Parsedown();
